41bbe53536c6af404577c1d3c2130c322c699919
Kikot Ekaterina <kati-kik@yandex.ru>
2023-06-05 10:09:49 +0300

    Решена задача 36

---
+++ Task36/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-99, 100);
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int sum = 0;
+
+for (int m = 1; m < array.Length; m+=2)
+sum = sum + array[m];
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях:{sum} ");
